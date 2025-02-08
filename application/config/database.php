b7b056da9e394c71fc2eba0430c0815e5fe5d8f2
Fahmi <fahmi4331@gmail.com>
2025-02-08 19:10:27 +0800

    root

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
+	'hostname' => '103.167.35.206',
 	'username' => 'root',
-	'password' => 'ypgs1234',
+	'password' => '',
 	'database' => 'jbsakad',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
