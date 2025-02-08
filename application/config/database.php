7e1ef8b2785a039b0b4d3c46999a033c62c599c1
Fahmi <fahmi4331@gmail.com>
2025-02-08 18:59:41 +0800

    ypgs1234

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '103.167.35.206',
 	'username' => 'root',
 	'password' => 'ypgs1234',
 	'database' => 'jbsakad',
